97856c9308d07579456b05c37756027db265a295
rfoxmich <fox@nscl.msu.edu>
2022-08-18 12:19:40 -0400

    Building structs that we'll use in the system

--- src/rings/mod.rs
+++ src/rings/mod.rs
@@ -1,2 +1,17 @@
+//! The *rings* module provides a database of known ringbuffer
+//! files and the stuff we need to know.  These include:
+//! 
+//! *  The paths to the files themselves.  While the ringmaster process 
+//! is being written so that those files are all in the same directory
+//! really clever people will have noticed that in non POSIX shared 
+//! memory systems, where this all gets done with mmap(2), there's
+//! nothing to stop you from making a ringbuffer in a subdirectory.
+//! Or, even a ringbuffer in some other directory tree if REGISTER
+//! understands that .
+//! *  Information about the clients that are know to be attached to
+//! those rings.  For the most part, that is the set of thread handles
+//! that represent threads that are monitoring client exits and
+//! the variable used to ask a thread to exit.  
+//! 
 pub mod rings;
 pub use self::rings::rings::*;
--- src/rings/rings.rs
+++ src/rings/rings.rs
@@ -1,1 +1,37 @@
-pub mod rings {}
+pub mod rings {
+    use std::sync;
+    use std::thread;
+    ///
+    /// This enum provides information about the
+    /// way a client is attached to a ring:
+    ///  
+    /// *  pid is the process id of the client.
+    /// *  slot is the consumer slot for a consumer client.
+    ///
+    #[derive(Copy, Clone, Debug)]
+    enum Client {
+        Producer { pid: u32 },
+        Consumer { pid: u32, slot: u32 },
+    }
+    ///
+    /// provides the information we need to know about a
+    /// ringmaster client monitor thread.
+    ///
+    /// *   handle -is the join handle for a monitor thread.
+    /// *   should_run - is the flag that will be initialized to ```true```
+    /// and set to false to request the thread exit.
+    ///
+    struct ClientMonitorInfo {
+        handle: thread::JoinHandle<()>,
+        should_run: sync::atomic::AtomicBool,
+        client_info: Client,
+    }
+    /// Provides all of the information we, the ringmaster, need to know
+    /// about a ringbuffer
+    ///
+    pub struct RingBufferInfo {
+        ring_file: String,
+        client_monitors: Vec<ClientMonitorInfo>,
+    }
+    
+}
