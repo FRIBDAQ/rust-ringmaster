7f86c09b4bfdc4a1c68ddc8ec1817ea064e1b32f
rfoxmich <fox@nscl.msu.edu>
2022-08-18 12:32:03 -0400

    Building structs that we'll use in the system

--- src/rings/rings.rs
+++ src/rings/rings.rs
@@ -9,7 +9,7 @@ pub mod rings {
     /// *  slot is the consumer slot for a consumer client.
     ///
     #[derive(Copy, Clone, Debug)]
-    enum Client {
+    pub enum Client {
         Producer { pid: u32 },
         Consumer { pid: u32, slot: u32 },
     }
@@ -21,10 +21,42 @@ pub mod rings {
     /// *   should_run - is the flag that will be initialized to ```true```
     /// and set to false to request the thread exit.
     ///
-    struct ClientMonitorInfo {
-        handle: thread::JoinHandle<()>,
+    pub struct ClientMonitorInfo {
+        handle: Option<thread::JoinHandle<()>>,
         should_run: sync::atomic::AtomicBool,
         client_info: Client,
+    }
+    impl ClientMonitorInfo {
+        ///
+        /// prepares a ClientMonitorInfo struct. Note that
+        /// we don't have a monitor thread yet.  This is
+        /// added by set_monitor.  This is necessary because we don't
+        /// want a race condition between setting up the should_run
+        /// atomic bool and the thread  referencing for the first time.
+        /// The thread needa that initialized but it does not need
+        /// its own thread handle.
+        ///
+        pub fn new(client: Client) -> ClientMonitorInfo {
+            ClientMonitorInfo {
+                handle: None,
+                should_run: sync::atomic::AtomicBool::new(true),
+                client_info: client,
+            }
+        }
+        ///
+        /// set_monitor should be called to receive the thread handle
+        /// from the thread::spawn call.  Normally this will be
+        /// look something like:
+        ///
+        /// ```
+        ///  use nscldaq_ringmaster::rings::rings::rings::clap*
+        ///  let some_client = Client::Producer{pid : 1234};  
+        ///  let info = ClientMonitorInfo::new(some_client);
+        ///  info.set_monitor(thread::spawn(|| {}));
+        /// ```
+        pub fn set_monitor(&mut self, handle: thread::JoinHandle<()>) {
+            self.handle = Some(handle);
+        }
     }
     /// Provides all of the information we, the ringmaster, need to know
     /// about a ringbuffer
@@ -33,5 +65,4 @@ pub mod rings {
         ring_file: String,
         client_monitors: Vec<ClientMonitorInfo>,
     }
-    
 }
