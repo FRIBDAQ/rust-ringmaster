5eaa68142b8de3e24591e2ee7d048ec24fb922dd
rfoxmich <fox@nscl.msu.edu>
2022-08-23 14:06:03 -0400

    Tested inventory_rings function

--- src/rings/inventory.rs
+++ src/rings/inventory.rs
@@ -21,7 +21,11 @@ pub mod inventory {
     /// The name of the file as a string slice reference is
     /// passed to each of those closures.
     ///
-    pub fn inventory_rings(dir_name: &str, is_ring: &Fn(&str), not_ring: &Fn(&str)) {
+    pub fn inventory_rings(
+        dir_name: &str,
+        is_ring: &mut dyn FnMut(&str),
+        not_ring: &mut dyn FnMut(&str),
+    ) {
         let path = Path::new(dir_name);
         let iteration = fs::read_dir(path).unwrap();
         for file in iteration {
@@ -32,6 +36,35 @@ pub mod inventory {
             } else {
                 not_ring(&name);
             }
+        }
+    }
+    #[cfg(test)]
+    mod inv_test {
+        use super::*;
+        use std::ffi::OsString;
+        use std::io;
+        use std::path::Path;
+        // Note that I _think_ the working dir is the project top dir
+        // if these are run via cargo.
+
+        // closures that are useful to tests:
+
+        fn collect_names(name: &str, collection: &mut Vec<String>) {
+            collection.push(String::from(name));
+        }
+        #[test]
+        fn inv_1() {
+            let mut not_rings = Vec::<String>::new();
+            let mut rings = Vec::<String>::new();
+            inventory_rings(
+                ".",
+                &mut |name| collect_names(name, &mut rings),
+                &mut |name| collect_names(name, &mut not_rings),
+            );
+            assert_eq!(1, rings.len());
+            let p = Path::new(&rings[0]);
+
+            assert!(p.ends_with("poop"));
         }
     }
 }
